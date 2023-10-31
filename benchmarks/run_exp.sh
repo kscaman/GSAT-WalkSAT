4ba859917db38803c7942901bca81eeb2ed11ccb
kscaman <99339644+kscaman@users.noreply.github.com>
2023-10-31 23:43:06 +0100

    modified run_exp.sh

--- benchmarks/run_exp.sh
+++ benchmarks/run_exp.sh
@@ -4,10 +4,10 @@ echo ">> by Kevin Scaman adapted from Sina M.Baharlou <<"
 echo ">> October 2023 <<"
 
 
-file="uf150/"
-logfile="log_uf150.txt"
-resultlog="log_uf150l-"
-iterations=$((10000))
+file="uf20/uf20-01.cnf"
+logfile="cdf.txt"
+resultlog="cdf-"
+n_samples=$((10000))
 flip_max=$((10000))
 rnd_method=$((-1))
 
@@ -17,10 +17,12 @@ gsat()
 	count=$((0));
 	echo ""> $logfile
 	start_time=$(date +%s%N) ;
-	echo 'Solving : '$file;
-	./GSAT -i $file --iterations $iterations --flip_max $flip_max --rnd_method $rnd_method --heuristic $heuristic_method  >> $logfile;
+	for i in $(seq 1 $n_samples);
+	do echo 'Solving : '$file' iteartion : '$i;
+	./GSAT -i $file --iterations 1 --flip_max $flip_max --rnd_method $rnd_method --heuristic $heuristic_method  >> $logfile;
 	count=$(($count+$?));
-	echo "\n\n">>$logfile
+	# echo "\n\n">>$logfile
+    done
 	
 	end_time=$((($(date +%s%N) - $start_time)/1000000)) ; 
 	echo "Time elapsed : $end_time ms." >> $resultlog$heuristic_method".txt"
